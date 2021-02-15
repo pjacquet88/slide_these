4bb6d3abdfbbff9ecb11280c858cec0c93ad5744
JACQUET Pierre <pierre.jacquet@inria.fr>
2021-02-15 10:46:12 +0100

    thesis V0

--- compile.sh
+++ compile.sh
@@ -5,11 +5,10 @@
 # main.nav main.toc main.log main.out main.snm
 
 
-pdflatex main.tex
-#bibtex main
-#pdflatex main.tex
-#pdflatex main.tex
-# evince main.pdf &
+pdflatex --synctex=1 main.tex
+bibtex main
+pdflatex --synctex=1 main.tex
+pdflatex --synctex=1 main.tex
 rm -rf main.aux main.log main.out main.toc main.bbl main.blg
 
 
---
+++ graph/compile.sh
@@ -0,0 +1,3 @@
+cd /home/pierre/Documents/Latex/Manuscript
+sh compile.sh
+cd -
